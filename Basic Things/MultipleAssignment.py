45aa825709410521bd4e8a603ea524caa720c888
rai-sahil <raisahil580@gmail.com>
2022-12-11 01:23:20 -0800

    Chapter 2 Finished

---
+++ Basic Things/MultipleAssignment.py
@@ -0,0 +1,15 @@
+# Multiple Assignment
+
+name, age, attrative = "Sahil", 19, False
+
+print(name)
+print(age)
+print(attrative)
+
+x = 2
+y = 9
+i = 9
+j = 11
+
+x = y = i = j
+print(x)
---
+++ Basic Things/Variable.py
@@ -0,0 +1,30 @@
+# How does a variable works in python.
+
+# Strings
+firstName = "Sahil"
+lastName = "Rai"
+fullName = firstName + " " + lastName
+
+# To print the data type of the functions
+print(type(fullName))
+
+print(fullName)
+
+# INT
+age = 19
+age += 1
+print(age)
+
+# Can't do this
+# print("Your age is " + (age))
+# instead do this
+print("Your age is " + str(age))
+
+# FLOAT
+height = 250.5
+print(height)
+print("Your Heigth is " + str(height) + "cm");
+
+#BOOLEAN
+human = True
+print("Are you a human: " + str(human))
