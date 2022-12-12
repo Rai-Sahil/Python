c3f7c6d7e050a004f2205b158da7dcf52c4f2058
rai-sahil <raisahil580@gmail.com>
2022-12-12 01:41:35 -0800

    Chapter 4 - String Methods - Done

---
+++ Basic Things/stringMeethods.py
@@ -0,0 +1,12 @@
+name = "Sahil rai"
+
+print(len(name))
+print(name.find("S")) # Find where capital B is?
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("a")) # Conut the number a in string
+print(name.replace("a", "s"))
+print(name * 3) # Display string multiple times.
